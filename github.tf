ba8df8e677ca76f73372e0999d5502a129dc31be
otofune <otofune@otofune.me>
2019-09-08 02:17:56 +0900

    :octocat: Add me

--- github.tf
+++ github.tf
@@ -1,3 +1,8 @@
+resource "github_membership" "github_member_otofune" {
+  username = "otofune"
+  role     = "admin"
+}
+
 resource "github_membership" "github_member_tosuke" {
   username = "Tosuke"
   role     = "member"

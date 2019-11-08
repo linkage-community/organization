ce5eccb28430b894d082bf23e880c8fa55a97903
otofune <otofune@otofune.me>
2019-11-08 21:44:53 +0900

    Add admin members

--- github.tf
+++ github.tf
@@ -5,5 +5,15 @@ resource "github_membership" "github_member_otofune" {
 
 resource "github_membership" "github_member_tosuke" {
   username = "Tosuke"
-  role     = "member"
+  role     = "admin"
+}
+
+resource "github_membership" "gm_rinsuki" {
+  username = "rinsuki"
+  role = "admin"
+}
+
+resource "github_membership" "gm_c" {
+  username = "ci7lus"
+  role = "admin"
 }
